059073f5fcdceb0fd57192c6e29c1138ff357869
Marco Meneghelli <mm@bendingspoons.com>
2017-09-08 18:54:14 +0200

    patch main

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -19,7 +19,6 @@ package com.example.android.android_me.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -53,13 +52,13 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         int bodyPartIndex = position % 12;
 
         switch (bodyPartIdetifier) {
-            case (0):
+            case 0:
                 headIndex = bodyPartIndex;
                 break;
-            case (1):
+            case 1:
                 bodyIndex = bodyPartIndex;
                 break;
-            case (2):
+            case 2:
                 legIndex = bodyPartIndex;
                 break;
         }
